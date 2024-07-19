4e53ee425c114d26f12257bb3bb9bd0368a2eab5
unknown <justinhoang0312@gmail.com>
2024-07-19 10:34:31 -0400

    Create dotenv to store sensitive information

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { nanoid } from "nanoid";
+import "dotenv/config";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 let posts = [];
 
 // Access body content of form input and JSON data

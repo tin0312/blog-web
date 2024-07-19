ab5d972ce12adf3089a62907486af147cf277bb8
unknown <justinhoang0312@gmail.com>
2024-07-19 09:52:50 -0400

    updating existing blog

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const app = express();
 const port = 3000;
 let posts = [];
 
-//access body content of form input
+// Access body content of form input and JSON data
 app.use(bodyParser.urlencoded({ extended: true }));
-// use static files
+app.use(bodyParser.json()); // Add this line to parse JSON data
+// Use static files
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -18,27 +19,37 @@ app.get("/", (req, res) => {
 app.get("/create-post", (req, res) => {
   res.render("create-post.ejs", { posts });
 });
+
 app.post("/my-posts", (req, res) => {
   posts.push({
     id: nanoid(),
     title: req.body.title,
     body: req.body.body,
   });
-  res.render("my-posts.ejs", {
-    posts,
-  });
+  res.render("my-posts.ejs", { posts });
 });
 
 app.get("/my-posts", (req, res) => {
-  res.render("my-posts.ejs", {
-    posts,
-  });
+  res.render("my-posts.ejs", { posts });
 });
+
 app.delete("/delete/:id", (req, res) => {
   const id = req.params.id;
   console.log("Delete request received for post ID:", id);
   posts = posts.filter((post) => post.id !== id);
   res.json({ success: true });
+});
+
+app.put("/update/:id", (req, res) => {
+  const id = req.params.id;
+  const index = posts.findIndex((post) => post.id === id);
+  if (index !== -1) {
+    posts[index].title = req.body.title;
+    posts[index].body = req.body.body;
+    res.json({ success: true });
+  } else {
+    res.json({ success: false });
+  }
 });
 
 app.listen(port, () => {

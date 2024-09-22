70825d1681573017d8fb23d25f7f8a8b3626f240
unknown <justinhoang0312@gmail.com>
2024-09-22 01:57:53 -0400

    Add process object to the browser environment to access environment variables

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,7 +1,13 @@
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = function override(config, env) {
   // Add the NodePolyfillPlugin to the plugins array
-  config.plugins.push(new NodePolyfillPlugin());
+  config.plugins.push(
+    new NodePolyfillPlugin(),
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    })
+  );
   return config;
 };

4b9f7a669447d74c95f9e2cee05a55c13de845fd
unknown <justinhoang0312@gmail.com>
2024-09-22 01:31:40 -0400

    Add polyfills for Node to allow path module usage in browser environment for using dotenv

---
+++ frontend/config-overrides.js
@@ -0,0 +1,7 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
+module.exports = function override(config, env) {
+  // Add the NodePolyfillPlugin to the plugins array
+  config.plugins.push(new NodePolyfillPlugin());
+  return config;
+};

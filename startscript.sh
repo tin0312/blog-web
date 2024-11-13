f2224a8882c7dc9e7231cf9e0a9140c622751d05
Hoang Nhat Truong <105316547+tin0312@users.noreply.github.com>
2024-11-13 16:51:18 -0500

    Update startscript.sh with nodes module installed

--- startscript.sh
+++ startscript.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
+
+# Navigate to blog-server directory and install dependencies
 cd ./blog-server
-#yarn ## Need to to install dependencies on first lunch
-rm -r ./clientbuild || true #remove previous build file
+npm install # Install server dependencies
+rm -r ./clientbuild || true # Remove previous build if it exists
 
+# Navigate to blog-client, install dependencies, build, and move to blog-server
 cd ../blog-client
-#yarn ## Need to to install dependencies on first lunch
-npm run build
-mv ./build ../blog-server/clientbuild
+npm install # Install client dependencies
+npm run build # Build the client
+mv ./build ../blog-server/clientbuild # Move the build to blog-server/clientbuild
 
+# Go back to blog-server and start the server
 cd ../blog-server
-
 npm start

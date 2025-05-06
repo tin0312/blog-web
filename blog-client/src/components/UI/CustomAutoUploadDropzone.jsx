55789d0082f87c2eddafba80068ecedb022d9cb3
tin0312 <justinhoang0312@gmail.com>
2025-05-06 11:30:15 -0400

    Prevent upload button from bubbling to the drop zone input to open the file picker

--- blog-client/src/components/UI/CustomAutoUploadDropzone.jsx
+++ blog-client/src/components/UI/CustomAutoUploadDropzone.jsx
@@ -2,53 +2,67 @@ import React, { useState } from "react";
 import { generateReactHelpers, useDropzone } from "@uploadthing/react";
 import { Button, Spinner } from "react-bootstrap";
 
-export default function CustomAutoUploadDropzone({ onUploadComplete, onError, imgFile, setImgFile, setIsImgInput }) {
-    const { useUploadThing } = generateReactHelpers()
+export default function CustomAutoUploadDropzone({
+    onUploadComplete,
+    onError,
+    imgFile,
+    setImgFile,
+    setIsImgInput,
+}) {
+    const { useUploadThing } = generateReactHelpers();
     const [isUploading, setIsUploading] = useState(false);
+
     const { startUpload } = useUploadThing("imageUploader", {
         onClientUploadComplete: (res) => {
-            setIsUploading(false)
-            onUploadComplete(res)
+            setIsUploading(false);
+            onUploadComplete(res);
         },
         onUploadError: (res) => {
-            setIsUploading(false)
-            onError(res)
+            setIsUploading(false);
+            onError(res);
         },
     });
 
     const onDrop = async (acceptedFiles) => {
         if (acceptedFiles.length > 0) {
             setIsImgInput(false);
-            setImgFile(acceptedFiles)
+            setImgFile(acceptedFiles);
         }
     };
 
-    const { getRootProps, getInputProps } = useDropzone({ onDrop });
-    const inputProps = imgFile.length > 0 ? { ...getInputProps(), disabled: true } : getInputProps();
+    const { getRootProps, getInputProps } = useDropzone({
+        onDrop,
+        disabled: isUploading
+    });
+
     return (
-        <div {...getRootProps()}
-            className={`w-75 p-4 mb-4 border rounded text-center cursor-pointer ${imgFile.length > 0 ? "bg-light" : ""
-                }`}
-            style={{ transition: "background-color 0.2s" }}>
-            <input {...inputProps} />
+        <div
+            {...getRootProps()}
+            className={`drag-zone-container w-75 p-4 mb-4 text-center cursor-pointer ${
+                imgFile.length > 0 ? "bg-light" : ""
+            }`}
+            style={{ transition: "background-color 0.2s" }}
+        >
+            <input {...getInputProps()} />
             {imgFile.length > 0 ? (
                 <>
                     <p className="mb-3">Drag & drop or upload to change your file here...</p>
-                    <Button variant="dark" onClick={async (e) => {
-                        e.stopPropagation();
-                        setIsUploading(true)
-                        await startUpload(imgFile)
-                    }}
+                    <Button
+                        variant="dark"
+                        onClick={(e) => {
+                            e.stopPropagation()
+                            setIsUploading(true);
+                            startUpload(imgFile);
+                        }}
                         disabled={isUploading}
-                    >{
-                            isUploading ?
-                                <Spinner animation="border" role="status">
-                                    <span className="visually-hidden">Loading...</span>
-                                </Spinner>
-
-                                :
-                                `Upload ${imgFile.length} files`
-                        }
+                    >
+                        {isUploading ? (
+                            <Spinner animation="border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </Spinner>
+                        ) : (
+                            `Upload ${imgFile.length} file${imgFile.length > 1 ? "s" : ""}`
+                        )}
                     </Button>
                 </>
             ) : (
